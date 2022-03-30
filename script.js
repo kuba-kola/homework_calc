4692d7f0646b991c703c3551b8f246d766f83306
Yakau Trafimovich <5243212@gmail.com>
2022-03-30 18:04:18 +0200

    upd

--- script.js
+++ script.js
@@ -3,9 +3,10 @@ const operationsBtn = document.querySelectorAll('.operations');
 const deleteBtn = document.querySelector('.delete');
 const equalsBtn = document.querySelector('.equals');
 const clearBtn = document.querySelector('.clear');
-const changeSignBtn = document.querySelector('.change-sign');
+const signChangeBtn = document.querySelector('.change-sign');
 const currentOperand = document.querySelector('[data-current-operand]');
 const previousOperand = document.querySelector('[data-previous-operand]');
+let newNumberFlag = false
 
 numbersBtn.forEach((button) => {
     button.addEventListener('click', enterNumber);
@@ -14,45 +15,6 @@ operationsBtn.forEach((button) => {
     button.addEventListener('click', enterOperation);
 });
 deleteBtn.forEach('click', deleteLastNumber);
-equalsBtn.forEach('click', enterEqualsBtn );
+equalsBtn.forEach('click', enterEquals);
 clearBtn.forEach('click', clearAll);
-changeSignBtn.forEach('click', signChange);
-
-function enterNumber(event) {
-    const newNumber = event.target.textContent;
-    const currentNumber = carrentOperand.value;
-
-    if (newNumberFlag) {
-        if (newNumber === '.') {
-            carrentOperand.value = '0.';
-            newNumberFlag = false;
-            return;
-        }
-
-        carrentOperand.value = newNumber;
-        newNumberFlag = false;
-        return;
-    }
-
-    if (newNumber === '.' && currentNumber.includes('.')) {
-    carrentOperand.value = currentNumber;
-    return;
-    }
-
-    if (currentNumber === '0' && newNumber === '.') {
-    carrentOperand.value = `${currentNumber}${newNumber}`;
-    return;
-    }
-
-    if (currentNumber === '0') {
-    carrentOperand.value = newNumber;
-    return;
-    }
-
-    carrentOperand.value = `${currentNumber}${newNumber}`;
-    
-
-}
-
-// class Calculator {
-// }
+signChangeBtn.forEach('click', signChange);
