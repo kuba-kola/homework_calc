b2eb9b3cdf9b9b9c077abcf49ea15762236e27dc
Yakau Trafimovich <5243212@gmail.com>
2022-03-30 13:49:18 +0200

    add enterNumber function

--- script.js
+++ script.js
@@ -1,10 +1,58 @@
 const numbersBtn = document.querySelectorAll('.numbers');
 const operationsBtn = document.querySelectorAll('.operations');
-const equalsBtn = document.querySelector('.equals');
 const deleteBtn = document.querySelector('.delete');
-const clearBtn = document.querySelector('.clear')
-const previousOperand = document.querySelector('[data-previous-operand]')
-const currentOperand = document.querySelector('[data-current-operand]')
+const equalsBtn = document.querySelector('.equals');
+const clearBtn = document.querySelector('.clear');
+const changeSignBtn = document.querySelector('.change-sign');
+const currentOperand = document.querySelector('[data-current-operand]');
+const previousOperand = document.querySelector('[data-previous-operand]');
 
-class Calculator {
+numbersBtn.forEach((button) => {
+    button.addEventListener('click', enterNumber);
+});
+operationsBtn.forEach((button) => {
+    button.addEventListener('click', enterOperation);
+});
+deleteBtn.forEach('click', deleteLastNumber);
+equalsBtn.forEach('click', enterEqualsBtn );
+clearBtn.forEach('click', clearAll);
+changeSignBtn.forEach('click', signChange);
+
+function enterNumber(event) {
+    const newNumber = event.target.textContent;
+    const currentNumber = carrentOperand.value;
+
+    if (newNumberFlag) {
+        if (newNumber === '.') {
+            carrentOperand.value = '0.';
+            newNumberFlag = false;
+            return;
+        }
+
+        carrentOperand.value = newNumber;
+        newNumberFlag = false;
+        return;
+    }
+
+    if (newNumber === '.' && currentNumber.includes('.')) {
+    carrentOperand.value = currentNumber;
+    return;
+    }
+
+    if (currentNumber === '0' && newNumber === '.') {
+    carrentOperand.value = `${currentNumber}${newNumber}`;
+    return;
+    }
+
+    if (currentNumber === '0') {
+    carrentOperand.value = newNumber;
+    return;
+    }
+
+    carrentOperand.value = `${currentNumber}${newNumber}`;
+    
+
 }
+
+// class Calculator {
+// }
