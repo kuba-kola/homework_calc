258ea45fe59c0d29b2cc8fdd398523cd613d9eb6
Yakau Trafimovich <5243212@gmail.com>
2022-03-30 12:55:33 +0200

    add querySelectors

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
-class Calculator {
+const numbersBtn = document.querySelectorAll('.numbers');
+const operationsBtn = document.querySelectorAll('.operations');
+const equalsBtn = document.querySelector('.equals');
+const deleteBtn = document.querySelector('.delete');
+const clearBtn = document.querySelector('.clear')
+const previousOperand = document.querySelector('[data-previous-operand]')
+const currentOperand = document.querySelector('[data-current-operand]')
 
+class Calculator {
 }
