201505fcd0672fd5ecf136df0735b3a6389bae01
Yakau Trafimovich <5243212@gmail.com>
2022-03-31 11:09:56 +0200

    add calculate

--- script.js
+++ script.js
@@ -11,6 +11,47 @@ let currentNumber = '';
 let lastNumber = '';
 let operation = undefined;
 
+const calculate = () => {
+  let calculation;
+
+  if(!lastNumber || !currentNumber) {
+    return;
+  }
+
+  const previous = parseFloat(lastNumber);
+  const current = parseFloat(currentNumber);
+
+  if(isNaN(previous) || isNaN(current)) {
+    return;
+  }
+
+  switch (operation) {
+    case '+':
+      calculation = previous + current;
+    break;
+    case '-':
+      calculation = previous - current;
+    break;
+    case '*':
+      calculation = previous * current;
+    break;
+    case '÷':
+      if(current === 0) {
+        clearResult();
+        return 0;
+      }
+
+      calculation = previous / current;
+    break;        
+    default:
+      return;
+  }
+
+  currentNumber = calculation;
+  operation = undefined;
+  lastNumber = '';  
+}
+
 numbersBtn.forEach((btn) => {
   btn.addEventListener('click', () => {
     enterNumber(btn.innerText);
